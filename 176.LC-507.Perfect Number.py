f4ccd65d0729b90b462c663619449e2baaef137a
rsivaraman18 <126255552+rsivaraman18@users.noreply.github.com>
2024-05-14 12:37:48 +0530

    Create 176.LC-507.Perfect Number.py

---
+++ 176.LC-507.Perfect Number.py
@@ -0,0 +1,36 @@
+# 176.LC-507.Perfect Number.py
+
+# def program(num):
+#     result = []
+#     for i in range(1,num):
+#         if num%i==0:
+#             result.append(i)
+#     tot  = sum(result)
+#     if tot == num:
+#         print('True')
+#         return True
+#     else:
+#         print('False')
+#         return False
+
+
+# program(28)
+# program(7)
+
+
+
+def program(num):
+    total = 0
+    for i in range(1,num):
+        if num%i==0:
+            total += i
+    if total == num:
+        print('True')
+        return True
+    else:
+        print('False')
+        return False
+
+
+program(28)
+program(7)
