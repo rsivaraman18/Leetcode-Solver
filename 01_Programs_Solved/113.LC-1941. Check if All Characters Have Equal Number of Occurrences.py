efbb8f570d390aac636cc91befc4d7866078a66e
rsivaraman18 <rsivaraman18@gmail.com>
2024-09-12 11:30:39 +0530

    5 Programs solved

---
+++ 01_Programs_Solved/113.LC-1941. Check if All Characters Have Equal Number of Occurrences.py
@@ -0,0 +1,20 @@
+
+def check(s):
+    s1 = set(s)
+    print(s1)
+    d = { }
+    for i in s1:
+        d[i] = 0
+    print(d)
+    for i in s:
+        d[i] = d[i] + 1
+    print(d)
+    for i in d.values():
+        
+        
+    
+
+
+
+
+check("abacbc")
