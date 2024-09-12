25119d351b369a86c2fb5f1d3e6b8ef90eef8dd8
rsivaraman18 <rsivaraman18@gmail.com>
2024-09-12 11:40:27 +0530

    Solutions

---
+++ 01_Programs_Solved/159.LC-1694 Reformat the Phone Number.py
@@ -0,0 +1,17 @@
+''' 1694. Reformat Phone Number '''
+
+def phone(number):
+    newphone = ''
+    for i in number:
+        if i.isdigit():
+            newphone =newphone+i
+    print(newphone)
+    while True:
+        new = newphone[0:4]
+        print(new)
+            
+    print(newphone)
+
+    
+
+phone(number = "1-23-45 6")
