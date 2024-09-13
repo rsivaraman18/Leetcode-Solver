e76271cc289dd16608971ae1fb11253aa452baf7
rsivaraman18 <rsivaraman18@gmail.com>
2024-09-13 15:23:07 +0530

    Program Solved

--- 03_Ready_practice/check.py
+++ 03_Ready_practice/check.py
@@ -1,1 +1,17 @@
-Hello
+def add_sprinkles(func):
+    def wrapper():
+        print("You added sprinkles")
+        func()
+    print('Outer Ends here')
+    return wrapper
+
+
+@add_sprinkles
+def get_icecream():
+    print("Here is your icecream")
+
+get_icecream
+
+
+####################################
+
