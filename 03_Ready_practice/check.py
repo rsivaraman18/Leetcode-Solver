7763dee50ee58498e344a48afeaa181df8c75675
rsivaraman18 <rsivaraman18@gmail.com>
2024-09-13 15:44:27 +0530

    done

--- 03_Ready_practice/check.py
+++ 03_Ready_practice/check.py
@@ -1,17 +1,94 @@
+#Example 1 : 
+
+# def add_sprinkles(func):
+#     def wrapper():
+#         print("You added sprinkles")
+#         func()
+#     print('Outer Ends here')
+#     return wrapper
+
+
+# @add_sprinkles
+# def get_icecream():
+#     print("Here is your icecream")
+
+# get_icecream
+
+
+####################################
+#Example 2 : Withou inner Function
+
+# def add_sprinkles(func):
+    
+#     print("You added sprinkles")
+#     func()
+
+
+# @add_sprinkles
+# def get_icecream():
+#     print("Here is your icecream")
+
+# get_icecream
+
+####################################
+# Example3:
+
 def add_sprinkles(func):
     def wrapper():
         print("You added sprinkles")
         func()
-    print('Outer Ends here')
+    print('Outer1 Ends here')
     return wrapper
 
 
+def add_choculates(func):
+    def wrapper():
+        print("You added choculates")
+        func()
+    print('Outer2 Ends here')
+    return wrapper
+
+
+
+
+
 @add_sprinkles
+@add_choculates
 def get_icecream():
     print("Here is your icecream")
 
-get_icecream
+get_icecream()
 
 
-####################################
+######################
+# EG4
 
+def add_sprinkles(func):
+    def wrapper(*args):
+        print("You added sprinkles")
+        func(*args)
+    print('Outer1 Ends here')
+    return wrapper
+
+
+def add_choculates(func):
+    def wrapper(*args):
+        print("You added choculates")
+        func(*args)
+    print('Outer2 Ends here')
+    return wrapper
+
+
+
+
+
+@add_sprinkles
+@add_choculates
+def get_icecream(flavour):
+    print(f"Here is your {flavour} icecream")
+
+get_icecream("Vanilla")
+
+
+
+ 
