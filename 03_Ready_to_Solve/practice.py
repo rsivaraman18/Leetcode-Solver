7413b643457697ae0bc494052b1d197c2dfa5d02
rsivaraman18 <rsivaraman18@gmail.com>
2024-09-13 10:07:49 +0530

    Python Practice

--- 03_Ready_to_Solve/practice.py
+++ 03_Ready_to_Solve/practice.py
@@ -1,31 +1,125 @@
-print("Ready")
-name = "siva"
-job = "Python developer"
+# print("Ready")
+# name = "siva"
+# job = "Python developer"
 
-print(f'My name is {name} and job is {job}')
-print("My name is {1} and job is {0}".format(job,name))
-print("My name is {a} and job is {b}".format(a=job,b=name))
+# print(f'My name is {name} and job is {job}')
+# print("My name is {1} and job is {0}".format(job,name))
+# print("My name is {a} and job is {b}".format(a=job,b=name))
 
-data = {
-            'name':'siva',
-            'job':'Developer',
-            'car':'scropio'
+# data = {
+#             'name':'siva',
+#             'job':'Developer',
+#             'car':'scropio'
 
-}
+# }
 
-for item in data.items():
-    x,y = item
-    print(x,'-->',y)
+# # for item in data.items():
+# #     x,y = item
+# #     print(x,'-->',y)
 
-for item in data.values():
-    print(item)
+# # for item in data.values():
+# #     print(item)
 
-for item in data.keys():
-    print(item)
+# # for item in data.keys():
+# #     print(item)
 
-print()
+# # print()
 
-def greet(**data):
-    print(data)
+# # def greet(**data):
+# #     print(data)
 
-greet(hello=124,nanban=232)
+# # greet(hello=124,nanban=232)
+
+
+# # price = 10
+
+# # def calculate():
+# #     price = price + 10
+# #     print(price)
+
+# # print(price)
+# # calculate()
+# # print(price)
+
+
+# # x = 8
+
+# # def loc():
+# #     x = 'local'
+# #     print('local x value ',x)
+
+# # loc()
+
+# # print('global x value',x)
+
+
+
+
+
+# # discount = 20
+# # tie = 99
+
+
+
+
+
+
+# # def f(x):
+# #     global x
+# #     x =10
+# #     print(x)
+
+# # f(15)
+
+
+# x= 12
+# x= 30
+
+
+# x = 15
+# def f(a,b=x):
+#     print(a,b)
+# x =222
+# f(4)
+
+
+# y,z = 1,2
+# def f():
+#     global x
+#     x = y + z
+#     print(x)
+
+# f()
+# print(x)
+
+# discount = 100
+# tie = 500
+
+# def odi():
+#     global discount
+#     discount = 30
+#     print('ot dis',discount)
+
+# def purchase():
+#     discount = 222222
+#     print(discount)
+#     print('--->',globals() ['tie'])
+#     print('--->',locals() ['discount'])
+
+
+# odi()
+# purchase()
+  
+
+# def fun(x):
+#     print('Function 1')
+#     return x()
+
+
+# def fun2():
+#     print('Hello Function 2')
+
+
+
+# print(fun(fun2))
+
---
+++ 03_Ready_to_Solve/practice_nested.py
@@ -0,0 +1,49 @@
+### FunCTIONS 1
+
+# def fun(x):
+#     print('Function 1')
+#     return x()
+
+
+# def fun2():
+#     print('Hello Function 2')
+
+
+
+# print(fun(fun2))
+
+# print('***************')
+
+######################################################
+# def fun(x):
+#     print('Function 1')
+#     return x
+
+
+# def fun2():
+#     print('Hello Function 2')
+
+
+
+# # print(fun(fun2))
+# fun(fun2)
+
+# print('***************')
+
+######################################################
+
+def fun1(x):
+    print('Function 1')
+    return x
+
+
+def fun2():
+    print('Hello Function 2')
+
+
+
+x = fun1(fun2)
+print(x())
+
+
+
