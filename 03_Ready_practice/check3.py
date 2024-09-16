752590ac532cdda68dc3811674208cc59cc2cc89
rsivaraman18 <rsivaraman18@gmail.com>
2024-09-16 15:03:54 +0530

    Program Solved

--- 03_Ready_practice/check3.py
+++ 03_Ready_practice/check3.py
@@ -6,16 +6,39 @@
 # print('Check3:',__name__)
 
 ###### 
-numbers = [1,2,11,22]
+# numbers = [1,2,11,22]
 
-def square(x):
-    if x >10:
-        return x**2
-    else:
-        return x**3
+# def square(x):
+#     if x >10:
+#         return x**2
+#     else:
+#         return x**3
     
-greater = list( map(square, numbers))
-big     = list(filter(square , numbers))
+# greater = list( map(square, numbers))
+# big     = list(filter(square , numbers))
 
-print('greater',greater)
-print('big',big)
+# print('greater',greater)
+# print('big',big)
+
+
+### REDUCE
+
+# from functools import reduce
+# numbers = [1,2,3,4,5]
+# numbers2 = [1,2,3,4,5]
+
+# def addition (x,y,z):
+#     return x+y+z
+
+# total = reduce(addition,numbers,numbers2)
+# print(total)
+
+from functools import reduce
+L = [1,2,43,4,5,10,23,15,7]
+m = reduce(lambda x,y : x if x>y else y , L)
+print(m)
+
+
+""" Hello functools"""
+print(__doc__)
+print(dir())
