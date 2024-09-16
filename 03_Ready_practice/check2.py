e43da8ba7b8cd9754c11aa6494058e1d2d86bde1
rsivaraman18 <rsivaraman18@gmail.com>
2024-09-16 12:40:47 +0530

    Program Solved

--- 03_Ready_practice/check2.py
+++ 03_Ready_practice/check2.py
@@ -19,11 +19,29 @@ class Greet:
         return f"Good Morning {self.name}"
 
     
-obj = Greet("Siva")
-print(obj.wish())
+# obj = Greet("Siva")
+# print(obj.wish())
 
-# print(dir())
-print('Check2:',__name__)
+# # print(dir())
+# print('Check2:',__name__)
 
+
+######################
+
+# L = [lambda x :x**2 , lambda x :x**3]
+# for f in L:
+#     print(f(3))
+
+
+######
+
+l = [n for n in range(5)]
+f = lambda x : bool(x%2)
+print(f(3),f(1))  ### True True
+
+for i in range(len(l)):
+    if f(l[i]):
+        del l[i]
+        print(i)
 
 
