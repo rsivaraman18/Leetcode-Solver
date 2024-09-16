c7d8703207b7e3c4acbdeeafc41b6f6f5019a424
rsivaraman18 <rsivaraman18@gmail.com>
2024-09-16 14:33:51 +0530

    Program Solved

--- 03_Ready_practice/check3.py
+++ 03_Ready_practice/check3.py
@@ -1,6 +1,21 @@
-import check2
-print("check3 ")
-print(check2.callme())
-print(check2.Greet)
+# import check2
+# print("check3 ")
+# print(check2.callme())
+# print(check2.Greet)
 
-print('Check3:',__name__)
+# print('Check3:',__name__)
+
+###### 
+numbers = [1,2,11,22]
+
+def square(x):
+    if x >10:
+        return x**2
+    else:
+        return x**3
+    
+greater = list( map(square, numbers))
+big     = list(filter(square , numbers))
+
+print('greater',greater)
+print('big',big)
