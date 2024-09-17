753b52a4c4d4d8e7b623a3bb3d2c329287d87fd3
rsivaraman18 <rsivaraman18@gmail.com>
2024-09-17 12:37:13 +0530

    Program Solved

---
+++ 03_Ready_practice/1_Nested.py
@@ -0,0 +1,226 @@
+#Example 1 : 
+
+# def add_sprinkles(func):
+#     def wrapper():
+#         print("You added sprinkles")
+#         func()
+#     print('Outer Ends here')
+#     return wrapper
+
+
+# @add_sprinkles
+# def get_icecream():
+#     print("Here is your icecream")
+
+# get_icecream()
+
+
+####################################
+#Example 2 : Withou inner Function
+
+# def add_sprinkles(func):
+    
+#     print("You added sprinkles")
+#     func()
+
+
+# @add_sprinkles
+# def get_icecream():
+#     print("Here is your icecream")
+
+# get_icecream
+
+
+
+####################################
+# Example3:
+
+# def add_sprinkles(func):
+#     def wrapper():
+#         print("You added sprinkles")
+#         func()
+#     print('Outer1 Ends here')
+#     return wrapper
+
+
+# def add_choculates(func):
+#     def wrapper():
+#         print("You added choculates")
+#         func()
+#     print('Outer2 Ends here')
+#     return wrapper
+
+
+
+
+
+# @add_sprinkles
+# @add_choculates
+# def get_icecream():
+#     print("Here is your icecream")
+
+# get_icecream()
+
+
+######################
+# EG4
+
+# def add_sprinkles(func):
+#     def wrapper(*args):
+#         print("You added sprinkles")
+#         func(*args)
+#     print('Outer1 Ends here')
+#     return wrapper
+
+
+# def add_choculates(func):
+#     def wrapper(*args):
+#         print("You added choculates")
+#         func(*args)
+#     print('Outer2 Ends here')
+#     return wrapper
+
+
+
+
+
+# @add_sprinkles
+# @add_choculates
+# def get_icecream(flavour):
+#     print(f"Here is your {flavour} icecream")
+
+# get_icecream("Vanilla")
+
+#################
+# Example 6
+# def make_pretty(func):
+#     def inner():
+#         print("I got decorated")
+#         func()
+#     return inner
+
+
+# def ordinary():
+#     print("I am ordinary")
+
+# # Output: I am ordinary
+
+
+####################
+# Example 5:
+# def smart_divide(func):
+#     def inner(a, b):
+#         print("I am going to divide", a, "and", b)
+#         if b == 0:
+#             print("Whoops! cannot divide")
+#             return
+
+#         return func(a, b)
+#     return inner
+
+# @smart_divide
+# def divide(a, b):
+#     print(a/b)
+
+# divide(2,5)
+
+# divide(2,0)
+
+
+########### Example 7
+
+# def star(func):
+#     def inner(*args, **kwargs):
+#         print(11111111)
+#         func(*args, **kwargs)
+#         print(2222222)
+#     print('star 1')
+#     return inner
+
+
+# def percent(func):
+#     def inner(*args, **kwargs):
+#         print(3333333)
+#         func(*args, **kwargs)
+#         print(444444)
+#     print('percent 2')
+#     return inner
+
+
+# @star
+# @percent
+# def printer(msg):
+#     print(msg)
+
+# printer("Hello")
+
+
+##### Example 7
+# def make_multiplier_of(n):
+#     def multiplier(x):
+#         return x * n
+#     return multiplier
+
+
+# # Multiplier of 3
+# times3 = make_multiplier_of(3)
+
+# # Multiplier of 5
+# times5 = make_multiplier_of(5)
+
+# # Output: 27
+# print(times3(9))
+
+# # Output: 15
+# print(times5(3))
+
+# # Output: 30
+# print(times5(times3(2)))
+
+### Example 8
+# def greet(name):
+#     return f"Hello & Welcome {name}"
+
+# gm = greet("Python")
+
+######### Example 9
+
+# def hello():
+#     ...
+#     def inner():
+#         ...
+
+############# Example 10
+
+# traffic_color = ["GREEN","YELLOW","RED"]
+# def is_valid_color(usercolor):
+#     if usercolor.lower() in list(map(str.lower,traffic_color)):
+#         return True
+
+# usercolor = "YEllOw"
+# result = is_valid_color(usercolor)
+# if result:
+#     print(f"You have choosen the right color '{usercolor}' ")
+# else:
+#     print(f"{usercolor} is not a valid Traffic Color")
+
+################## Example 11
+
+class Subjects:
+
+    def maths():
+        print("Calculation Involved")
+    
+    def tamil():
+        print("Language to communicate")
+
+    def science():
+        print("Understand the Natures Effect")
+    
+    def social():
+        print("Learn the History !!!")
+
+
+student = Subjects
+student.maths() 
+
