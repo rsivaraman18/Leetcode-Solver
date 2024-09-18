aba392ed90cc00eb7b132987744522a47d1f00ae
rsivaraman18 <rsivaraman18@gmail.com>
2024-09-18 12:16:56 +0530

    Program Solved

---
+++ 02_Error_On_Attempts/1436 Destination City.py
@@ -0,0 +1,16 @@
+def destCity(paths):
+    for path in paths:
+        destination = path[1]
+    return(destination)
+
+
+# destCity(paths = [["London","New York"],["New York","Lima"],["Lima","Sao Paulo"]])
+# destCity( paths = [["B","C"],["D","B"],["C","A"]])
+# destCity(paths = [["A","Z"]])
+destCity([["pYyNGfBYbm","wxAscRuzOl"],["kzwEQHfwce","pYyNGfBYbm"]])
+
+## Exepected "wxAscRuzOl"
+
+
+
+
